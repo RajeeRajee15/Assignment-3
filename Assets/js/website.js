c1f2b9b9883d5736f56f58d490fc0ed351d77321
Rajee <rajeevan@gmail.com>
2023-11-13 22:02:40 +0530

    chang

---
+++ Assets/js/website.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+ }
+
+
+ // map
+function initMap() {
+    var options= {
+      zoom:20,
+      center:{lat:9.657770,lng:80.159210}
+    }
+    var googlemap = new google.maps.Map(document.getElementById("map"), options);
+  }
+
+
+
+
